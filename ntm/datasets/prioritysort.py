94dc39bed114dc8b3d6f97d2b689ab100f1f76fc
Giovanni De Toni <giovanni.det@gmail.com>
2019-09-18 09:38:26 +0200

    Add non-uniform priority to the priority sort task.

--- ntm/datasets/prioritysort.py
+++ ntm/datasets/prioritysort.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import Dataset
 from torch.distributions.uniform import Uniform
+from torch.distributions.beta.Beta import Beta
 from torch.distributions.binomial import Binomial
 
 
@@ -25,6 +26,7 @@ class PrioritySort(Dataset):
         self.seq_width = task_params["seq_width"]
         self.input_seq_len = task_params["input_seq_len"]
         self.target_seq_len = task_params["target_seq_len"]
+        self.uniform = task_params["uniform"]
 
     def __len__(self):
         # sequences are generated randomly so this does not matter
@@ -44,7 +46,11 @@ class PrioritySort(Dataset):
         # [low, high) but in the paper the priorities are drawn from [-1,1].
         # This minor difference is being ignored here as supposedly it doesn't
         # affects the task.
-        priority = Uniform(torch.tensor([-1.0]), torch.tensor([1.0]))
+        if not self.uniform:
+            priority = Beta(torch.tensor([2.0]), torch.tensor([5.0]))
+        else:
+            priority = Uniform(torch.tensor([-1.0]), torch.tensor([1.0]))
+
         for i in range(self.input_seq_len):
             input_seq[i, self.seq_width] = priority.sample()
 
