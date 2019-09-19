46eef7425d61082d69f97474a2945682088e7617
Giovanni De Toni <giovanni.det@gmail.com>
2019-09-19 16:55:34 +0200

    Add multi layer controller (two LSTM).

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ dataset = PrioritySort(task_params)
 #dataset = NGram(task_params)
 #dataset = PrioritySort(task_params)
 
-saved_model_name = "priority_sort_{}_{}_{}_{}_{}_{}_{}_{}.pt".format(
+saved_model_name = "priority_sort_{}_{}_{}_{}_{}_{}_{}_{}_{}.pt".format(
     task_params['seq_width'] + 1,
     task_params['seq_width'],
     task_params['controller_size'],
@@ -50,11 +50,12 @@ saved_model_name = "priority_sort_{}_{}_{}_{}_{}_{}_{}_{}.pt".format(
     task_params['memory_unit_size'],
     task_params['num_heads'],
     task_params['uniform'],
-    task_params['random_distr']
+    task_params['random_distr'],
+    task_params['multi_layer_controller']
 )
 
 # Output directory for tensorboard
-configure(args.tb_dir+"/priority_sort_{}_{}_{}_{}_{}_{}_{}_{}".format(
+configure(args.tb_dir+"/priority_sort_{}_{}_{}_{}_{}_{}_{}_{}_{}".format(
     task_params['seq_width'] + 1,
     task_params['seq_width'],
     task_params['controller_size'],
@@ -62,7 +63,8 @@ configure(args.tb_dir+"/priority_sort_{}_{}_{}_{}_{}_{}_{}_{}".format(
     task_params['memory_unit_size'],
     task_params['num_heads'],
     task_params['uniform'],
-    task_params['random_distr']
+    task_params['random_distr'],
+    task_params['multi_layer_controller']
 ))
 
 
@@ -78,7 +80,8 @@ ntm = NTM(input_size=task_params['seq_width'] + 1,
           controller_size=task_params['controller_size'],
           memory_units=task_params['memory_units'],
           memory_unit_size=task_params['memory_unit_size'],
-          num_heads=task_params['num_heads'])
+          num_heads=task_params['num_heads'],
+          multi_layer_controller=task_params['multi_layer_controller'])
 
 criterion = nn.BCELoss()
 # As the learning rate is task specific, the argument can be moved to json file
@@ -100,7 +103,7 @@ args.saved_model = 'saved_model_prioritysort.pt'
 '''
 
 # Path for the model
-PATH = args.output_dir+"/priority_sort_{}_{}_{}_{}_{}_{}_{}_{}/".format(
+PATH = args.output_dir+"/priority_sort_{}_{}_{}_{}_{}_{}_{}_{}_{}/".format(
     task_params['seq_width'] + 1,
     task_params['seq_width'],
     task_params['controller_size'],
@@ -108,7 +111,8 @@ PATH = args.output_dir+"/priority_sort_{}_{}_{}_{}_{}_{}_{}_{}/".format(
     task_params['memory_unit_size'],
     task_params['num_heads'],
     task_params['uniform'],
-    task_params['random_distr']
+    task_params['random_distr'],
+    task_params['multi_layer_controller']
 )
 # Check if the directory exists. Create it otherwise
 if not os.path.isdir(PATH):
